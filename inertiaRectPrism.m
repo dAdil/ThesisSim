72c37802e945a0f13f22c70b869f71bdb6330d3d
dAdil <damian.adil@gmail.com>
2016-04-22 22:46:55 +1000

    Added inertiaRectPrism for calculating moment of inertias of rectangular prisms.

---
+++ inertiaRectPrism.m
@@ -0,0 +1,7 @@
+% Calculates the moments of inertia of a rectangular prism. 
+% http://www.eng.auburn.edu/~marghitu/MECH2110/C_4.pdf
+function [Ixx, Iyy, Izz] = inertiaRectPrism(a,b,c,m)
+	Ixx = 1/12 * m * (b^2 + c^2);
+	Iyy = 1/12 * m * (a^2 + c^2);
+	Izz = 1/12 * m * (a^2 + b^2);
+end
