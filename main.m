e1c7ed1a2fb64743aa6a617fac6944f7f6f3c6ca
dAdil <damian.adil@gmail.com>
2016-04-24 02:31:50 +1000

    Work on main

--- main.m
+++ main.m
@@ -7,6 +7,17 @@ clc;
 
 addpath('CommonFunctions');
 
+% Aircraft data and constants
+global g m Ixx Iyy Izz Ixz;
+g = 9.81;
+
+m = 0.840;
+
+Ixx = 0;
+Iyy = 0;
+Izz = 0;
+Ixz = 0;
+
 % Initial conditions
 phi0   	= 10.0*pi/180.0;
 theta0 	= 0.0*pi/180.0;
@@ -15,18 +26,9 @@ psi0   	= -10.0*pi/180.0;
 dt 	= 0.01;
 t 	= 0:dt:10;
 
-% Gains
-P_a = 10; P_e = P_a; P_r = P_a;
-I_a = 1; I_e = I_a; I_r = I_a; sum_a = 0; sum_e = 0; sum_r = 0;
-D_a = 5; D_e = D_a; D_r = D_a; rx_prev = 0; ry_prev = 0; rz_prev = 0;
-p = 0.0; q = 0.0; r = 0.0;
+% Initial state vector 
+% u,v,w,p,q,r,q0,q1,q2,q3,x,y,z
 
-% Aircraft data
-Ixx 	= 1.0;
-Iyy 	= 0.9;
-Izz 	= 1.1;
-
-% 
-quat_actual = e2q([phi0,theta0,psi0]);
+X = [0,0,0,0,0,0,[e2q([phi0;theta0;psi0])]',0,0,0]';
 
 
