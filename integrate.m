192f0ac872179102fd7a5c37cde2096893358d58
dAdil <damian.adil@gmail.com>
2016-04-24 23:32:13 +1000

    Debug integrate

--- integrate.m
+++ integrate.m
@@ -1,6 +1,6 @@
 % Runge-Kutta Integration
 % http://web.aeromech.usyd.edu.au//flightm/aero3500/Week_11_transparencies2.pdf
-function Y = integrate(X,U,Xdot0,dt)
+function Y = integrate(X,U,dt)
 	An = stateRates(X,U) * dt;
 	Bn = stateRates(X + An / 2, U) * dt;
 	Cn = stateRates(X + Bn / 2, U) * dt;
