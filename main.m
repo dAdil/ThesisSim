06fa602546b6eaffbcb29ce45a29e0b7a9ef997c
dAdil <damian.adil@gmail.com>
2016-04-25 12:24:12 +1000

    Added call to plotAngles and changed initial conditions

--- main.m
+++ main.m
@@ -21,9 +21,9 @@ Izz = inertia(3);
 Ixz = inertia(5);
 
 % Initial conditions
-phi0   	= 10.0*pi/180.0;
-theta0 	= 0.0*pi/180.0;
-psi0   	= -10.0*pi/180.0;
+phi0   	= 0*pi/180.0;
+theta0 	= 90.0*pi/180.0;
+psi0   	= 0.0*pi/180.0;
 
 dt 	= 0.01;
 t 	= 0:dt:10;
@@ -40,4 +40,4 @@ for i = 1:length(t)
 	fprintf(fileID,"%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%4.4f,%d,%d,%d,%d,%4.4f\n",X(1),X(2),X(3),X(4),X(5),X(6),X(7),X(8),X(9),X(10),X(11),X(12),X(13),U(1),U(2),U(3),U(4),t(i));
 end
 
-
+plotAngles();
